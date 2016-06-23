a465c83f1d95254524a0f2d3b0e75d34b8c3459a
antevis <polyconnect@me.com>
2016-06-23 08:29:48 +0300

    Lightning mode implemented

--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import GameKit
 import AudioToolbox
 
 //Entirely borrowed this elegant enum by Pasan
@@ -27,19 +26,22 @@ enum ColorComponents {
 }
 
 class ViewController: UIViewController {
-    
-    let questionsPerRound = 4
 
-	var correctAnswers: Int?// = 0
+	var correctAnswers: Int = 0
 	
-	var indexOfSelectedQuestion: Int?
+	var questionIndex: Int = 0
 	
 	var questions: [Question]?
 	var questionItem: Question?
     
     var gameSound: SystemSoundID = 0
 	
+	var lightningMode: Bool = true
+	
 	let triviaModel = TriviaModel()
+	
+	var currentQuestionStatus: (qIndex: Int, answered: Bool) = (0, false)
+	
 	
     @IBOutlet weak var questionField: UILabel!
 	@IBOutlet weak var infoLabel: UILabel!
@@ -54,7 +56,7 @@ class ViewController: UIViewController {
 	let wrongAnswerLabelColor = ColorComponents.RGB(red: 254, green: 147, blue: 81, alpha: 1)
 	let dimmedFuncButtonColor = ColorComponents.RGB(red: 10, green: 81, blue: 72, alpha: 1)
 	let dimmedFuncButtonTitleColor = ColorComponents.RGB(red: 170, green: 170, blue: 170, alpha: 1)
-	
+	let whiteColor = ColorComponents.RGB(red: 255, green: 255, blue: 255, alpha: 1)
 	
 	
     override func viewDidLoad() {
@@ -74,7 +76,7 @@ class ViewController: UIViewController {
 		
 		removeButtonsFrom(buttonStack)
 		
-		processParamsFor(button: funcButton, color: dimmedFuncButtonColor, titleColor: dimmedFuncButtonTitleColor, enabled: false)
+		setParamsFor(button: funcButton, color: dimmedFuncButtonColor, titleColor: dimmedFuncButtonTitleColor, enabled: false)
 		
 		if let questions = questions {
 			
@@ -93,15 +95,22 @@ class ViewController: UIViewController {
 						addAnswerOptionButtonTo(stack: buttonStack, text: options[i].optionText, tag: i, color: questionButtonColor)
 					}
 				}
+				
+				currentQuestionStatus = (questionIndex, false)
+				
+				if lightningMode {
+					
+					loadNextRoundWithDelay(seconds: 5)
+				}
 			}
 		}
 	}
 	
-	func processParamsFor(button button: UIButton, color: ColorComponents, titleColor: ColorComponents, enabled: Bool) {
+	func setParamsFor(button button: UIButton, color: ColorComponents, titleColor: ColorComponents, enabled: Bool) {
 		
 		button.backgroundColor = color.color()
-		funcButton.setTitleColor(titleColor.color(), forState: .Normal)
-		funcButton.enabled = enabled
+		button.setTitleColor(titleColor.color(), forState: .Normal)
+		button.enabled = enabled
 	}
 	
 	func displayGameOver(questionsCount: Int) {
@@ -110,25 +119,16 @@ class ViewController: UIViewController {
 		
 		var messageText = ""
 		
-		if let correctAnswers = correctAnswers {
-			
-			loadGameOverSound()
-			playSound()
-			
-			messageText = "Way to go!\nYou got \(correctAnswers) out of \(questionsCount) correct!"
-			
-			
-		} else {
-			
-			loadErrorSound()
-			playSound()
-			
-			messageText = "Well, it's embarrassing, but the number of correct answers couldn't be retreived."
-		}
+		loadGameOverSound()
+		playSound()
+		
+		messageText = "Way to go!\nYou got \(correctAnswers) out of \(questionsCount) correct!"
 		
 		questionField.text = messageText
 		
 		funcButton.setTitle("Play Again", forState: .Normal)
+		
+		setParamsFor(button: funcButton, color: correctColor, titleColor: whiteColor, enabled: true)
 	}
 	
 	
@@ -139,10 +139,10 @@ class ViewController: UIViewController {
 		
 		questions = triviaModel.shuffledQuestions()
 		
-		indexOfSelectedQuestion = 0
+		questionIndex = 0
 		correctAnswers = 0
 		
-		displayQuestion(indexOfSelectedQuestion!)
+		displayQuestion(questionIndex)
 		
 		funcButton.setTitle("Next Question", forState: .Normal)
     }
@@ -153,9 +153,9 @@ class ViewController: UIViewController {
 		loadNextQuestionSound()
 		playSound()
 		
-		indexOfSelectedQuestion? += 1
+		questionIndex += 1
 		
-		if let questions = questions, questionIndex = indexOfSelectedQuestion {
+		if let questions = questions {
 			
 			//Next question
 			if questionIndex < questions.count {
@@ -179,19 +179,23 @@ class ViewController: UIViewController {
     
     func loadNextRoundWithDelay(seconds seconds: Int) {
 		
-        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
-        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
-        // Calculates a time value to execute the method given current time and delay
-        let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, delay)
-        
-        // Executes the nextRound method at the dispatch time on the main queue
-        dispatch_after(dispatchTime, dispatch_get_main_queue()) {
-            //self.nextRound()
+		let qIndexBeforeDelay = currentQuestionStatus.qIndex
 			
+		// Converts a delay in seconds to nanoseconds as signed 64 bit integer
+		let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+		// Calculates a time value to execute the method given current time and delay
+		let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, delay)
+		
+		// Executes the nextRound method at the dispatch time on the main queue
+		dispatch_after(dispatchTime, dispatch_get_main_queue()) {
+			//self.nextRound()
 			
-			self.processFuncButtonAction()
-			
-        }
+			if self.lightningMode && (qIndexBeforeDelay == self.currentQuestionStatus.qIndex && !self.currentQuestionStatus.answered) {
+	
+				self.processFuncButtonAction()
+			}
+		}
+		
     }
 	
 	func soundUrlFor(file fileName: String, ofType: String) -> NSURL {
@@ -290,6 +294,8 @@ class ViewController: UIViewController {
 	
 	func answersButtonAction(sender: UIButton!) {
 		
+		currentQuestionStatus.answered = true
+		
 		if let questionItem = questionItem  {
 			
 			if let correct = questionItem.isCorrectOption(sender.tag) {
@@ -301,7 +307,7 @@ class ViewController: UIViewController {
 					
 					processLabel(infoLabel, text: "Correct!", textColor: correctColor)
 					
-					correctAnswers? += 1
+					correctAnswers += 1
 					
 				} else {
 					
@@ -328,7 +334,7 @@ class ViewController: UIViewController {
 		
 		repaintButtonsExcept(buttonStack, pickedTag: sender.tag)
 		
-		processParamsFor(button: funcButton, color: correctColor, titleColor: .RGB(red: 255, green: 255, blue: 255, alpha: 1), enabled: true)
+		setParamsFor(button: funcButton, color: correctColor, titleColor: .RGB(red: 255, green: 255, blue: 255, alpha: 1), enabled: true)
 	}
 }
 
